22306372e25e3a10420a3d0acf4a8395ad8d2f20
guicarlotto <carlotto.escritor@gmail.com>
2024-06-21 11:18:48 -0300

    Setting the JS.

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const form = document.querySelector("[data-js-form]");
 const template = document.querySelector("[data-js-template]");
+const wrapper = document.querySelector("[data-js-wrapper]");
 
 function handleSubmit(event) {
     event.preventDefault();
 
     const rating = new FormData(event.target).get("rating");
     console.log(rating);
+
+    if (rating) {
+        wrapper.innerHTML = template.innerHTML.replace(/{{ rating }}/, rating); 
+        // using the sintaxe {{ rating }} to identify the variable on HTML and replace it.
+    }
 }
 
 form.addEventListener("submit", handleSubmit);
